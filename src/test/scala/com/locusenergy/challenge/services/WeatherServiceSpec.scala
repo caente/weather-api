7bd91aa3d33cbe2c8a37d3a8c14685c7eb06dea5
Miguel A Iglesias <caente@gmail.com>
2014-03-07 11:51:07 -0500

    more specific tests

--- src/test/scala/com/locusenergy/challenge/services/WeatherServiceSpec.scala
+++ src/test/scala/com/locusenergy/challenge/services/WeatherServiceSpec.scala
@@ -25,21 +25,25 @@ class WeatherServiceSpec extends Specification with Specs2RouteTest with Weather
 
     "returns the rain historic" in {
       Get("/weather/rain?start_date=2014-03-01&end_date=2014-03-7&zip=33020") ~> myRoute ~> check {
-        responseAs[Historic].values.size === 6
+        responseAs[Historic] === Historic(
+          List("2014-03-01", "2014-03-02", "2014-03-03", "2014-03-04", "2014-03-05", "2014-03-06"),
+          List(0.0, 0.0, 0.0, 0.0, 0.65, 0.36))
         status === OK
       }
     }
 
     "returns the temperature historic" in {
       Get("/weather/temperature?start_date=2014-03-01&end_date=2014-03-7&zip=33020") ~> myRoute ~> check {
-        responseAs[Historic].values.size === 6
+        responseAs[Historic] === Historic(
+          List("2014-03-01", "2014-03-02", "2014-03-03", "2014-03-04", "2014-03-05", "2014-03-06"),
+          List(69.1, 73.0, 73.5, 72.5, 75.7, 73.0))
         status === OK
       }
     }
 
     "returns the sunny days percent" in {
       Get("/weather/sun?start_date=2014-03-01&end_date=2014-03-7&zip=33020") ~> myRoute ~> check {
-        responseAs[SunStatus].percent_sunny > 0
+        responseAs[SunStatus] === SunStatus(67.0)
         status === OK
       }
     }
